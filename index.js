97dbceab9e66988bf0ed758bfa360482d251d580
Mohitkumar Talreja <62738399+mathematical-guy@users.noreply.github.com>
2024-09-27 23:50:16 +0530

    Added basic express setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const router = express.Router();
+
+const path = __dirname + '/views/';
+const port = 8080;
+
+
+// Writing custom middlewares
+router.use(function (req, res, next) {
+    console.log('/' + req.method);
+    next();
+    console.log('After request is processed...')
+});
+
+router.get('/', function (req, res) {
+    res.json({"message": "Hello World 😂"});
+    console.log(path);
+});
+
+
+app.use(express.static(path));
+app.use('/', router);
+// app.use('json')
+
+app.listen(port, function () {
+  console.log('Example app listening on port 8080!')
+})
+
